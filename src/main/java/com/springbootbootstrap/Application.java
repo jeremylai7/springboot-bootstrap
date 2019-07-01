2aeeb02fd257bc48ba9746dfaebb4ed65643cbc9
jeremylai7 <1049024592@qq.com>
2019-07-01 14:20:00 +0800

    添加打包支持

--- src/main/java/com/springbootbootstrap/Application.java
+++ src/main/java/com/springbootbootstrap/Application.java
@@ -2,14 +2,21 @@ package com.springbootbootstrap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan("com.springbootbootstrap.dao")
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
 }
