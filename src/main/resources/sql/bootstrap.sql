5ca93925bc83f8a6f08a7f66659855cb9f0b810d
jeremylai7 <1049024592@qq.com>
2021-06-03 14:25:04 +0800

    添加user数据

--- src/main/resources/sql/bootstrap.sql
+++ src/main/resources/sql/bootstrap.sql
@@ -9,3 +9,11 @@ CREATE TABLE `t_user` (
   `user_type` varchar(16) COLLATE utf8_bin DEFAULT NULL COMMENT '用户类型',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB
+
+INSERT INTO `t_user` VALUES ('1', '1', 'apple', 'fe895fc2989c3ff0af142b62d3be3a8d98a923', '18', '0', null);
+INSERT INTO `t_user` VALUES ('2', '1', 'apple1', '133', 'fe895fc2989c3ff0af142b62d3be3a8d98a923', '0', 'NL');
+INSERT INTO `t_user` VALUES ('3', '1', 'apple2', '4', 'fe895fc2989c3ff0af142b62d3be3a8d98a923', '0', 'NL');
+INSERT INTO `t_user` VALUES ('4', '1', 'apple3', '464', 'fe895fc2989c3ff0af142b62d3be3a8d98a923', '0', 'NL');
+INSERT INTO `t_user` VALUES ('9', '1', 'apple4', '4646', 'fe895fc2989c3ff0af142b62d3be3a8d98a923', '0', 'NL');
+INSERT INTO `t_user` VALUES ('10', '8', 'apple5', '646', 'fe895fc2989c3ff0af142b62d3be3a8d98a923', '0', 'NL');
+INSERT INTO `t_user` VALUES ('11', '8', '46464', '46464', 'fe895fc2989c3ff0af142b62d3be3a8d98a923', '0', 'NL');
